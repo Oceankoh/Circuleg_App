e43f1916db92bbd4350c0daa6aaf78a052cbb5ba
Ocean <riverkex@gmail.com>
2020-08-05 00:58:59 +0800

    added basic skeleton layout
    added update function to update value of ir on the screen. Actual retreving of IR value yet to be done

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -1,13 +1,45 @@
+import 'package:circulegapp/Bluetooth.dart';
+import 'package:circulegapp/Globals.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class MainPage extends StatefulWidget{
+class MainPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => MainPageState();
 }
 
-class MainPageState extends State<MainPage>{
+class MainPageState extends State<MainPage> {
+  String result = "awating data";
+  BTcontroller btCtrl = BTcontroller();
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    update();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          //TODO insert graph
+          Center(
+            child: Text(
+              "Blood Flow Level:" + result,
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .body1,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
+    );
+  }
+
+  void update() async {
+    btCtrl.getValue().then((pt) {
+      setState(() {
+        result = pt.infraRed.toString();
+      });
+    });
   }
 }
