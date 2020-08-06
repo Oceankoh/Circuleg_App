b6524f7fa263738058dcf679aa5bfc5782228a83
Ocean <0c34nk0h@gmail.com>
2020-08-07 02:50:57 +0800

    UI stuff. - added connect to bluetooth button - and some ui formatting code

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -4,31 +4,59 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:circulegapp/Graph.dart';
 
+import 'Bluetooth.dart';
+import 'Globals.dart';
+import 'Globals.dart';
+import 'Globals.dart';
+
 class MainPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => MainPageState();
 }
 
 class MainPageState extends State<MainPage> {
-  String _result = "awating data";
-  BTcontroller _btCtrl = BTcontroller();
+  String _result = "Awaiting data";
+//  BTcontroller _btCtrl = BTcontroller();
 
   @override
   Widget build(BuildContext context) {
     _update();
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Circuleg Mobile Application"),
+      ),
       body: Column(
         children: <Widget>[
-          Graph(),
-          Center(
+          MaterialButton(
             child: Text(
-              "Blood Flow Level:" + _result,
-              style: Theme.of(context).textTheme.body1,
-              textAlign: TextAlign.center,
+              "Bluetooth Connect",
+              style: Theme.of(context).textTheme.button,
             ),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ConnectBluetooth()));
+            },
+            color: Colors.blueAccent,
+          ),
+          Graph(),
+          Row(
+            children: <Widget>[
+              Center(
+                child: Text(
+                  "Blood Flow Level: ",
+                  style: Theme.of(context).textTheme.body1,
+                ),
+              ),
+              Center(
+                child: Text(
+                  _result,
+                  style: Theme.of(context).textTheme.body1,
+                ),
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           ),
         ],
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       ),
     );
   }
@@ -39,6 +67,6 @@ class MainPageState extends State<MainPage> {
 //        result = pt.infraRed.toString();
 //      });
 //    });
-    _btCtrl.test();
+//    _btCtrl.test();
   }
 }
